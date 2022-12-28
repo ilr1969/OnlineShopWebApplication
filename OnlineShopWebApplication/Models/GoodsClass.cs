1600f45fe471ac461a8bbf39c00dc91b76dd8a53
Дмитрий Савченко <ilr1969@yandex.ru>
2022-12-28 11:38:33 +0300

    Вывод товаров на главной странице

---
+++ OnlineShopWebApplication/Models/GoodsClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApplication.Models
+{
+    public class GoodsClass
+    {
+        private static int ID_counter = 1;
+        public int ID;
+        public string Name;
+        public decimal Cost;
+        public string Description;
+
+        public GoodsClass(string name, int cost, string descr)
+        {
+            ID = ID_counter;
+            Name = name;
+            Cost = cost;
+            Description = descr;
+            ID_counter++;
+        }
+
+        public override string ToString()
+        {
+            return $"{ID}\n{Name}\n{Cost}\n{Description}".ToString();
+        }
+    }
+}
