077e044aadd67963b621e711eb6f6efa49141054
Дмитрий Савченко <ilr1969@yandex.ru>
2023-01-12 10:54:28 +0300

    Реализованы карточки товара.

--- OnlineShopWebApplication/ProductStorage.cs
+++ OnlineShopWebApplication/ProductStorage.cs
@@ -12,6 +12,8 @@ namespace OnlineShopWebApplication
             new ProductClass("Lambo", 25000000, "best"),
             new ProductClass("Camaro", 5000000, "good"),
             new ProductClass("Mustang", 7000000, "good"),
+            new ProductClass("Volga", 7000, "not bad"),
+            new ProductClass("Kopeyka", 700, "foo"),
         };
 
         public List<ProductClass> GetAll()
@@ -19,16 +21,9 @@ namespace OnlineShopWebApplication
             return productsList;
         }
 
-        public string TryGetByID(int id)
+        public ProductClass TryGetByID(int id)
         {
-            try
-            {
-                return productsList.First(product => product.ID == id).ToString();
-            }
-            catch (System.Exception)
-            {
-                return "Такого товара не существует!";
-            }
+            return productsList.First(product => product.ID == id);
         }
     }
 }
