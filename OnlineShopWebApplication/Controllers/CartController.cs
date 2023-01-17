13d0d5fd5d3d884d44b0849d1662940d1fe09892
Дмитрий Савченко <ilr1969@yandex.ru>
2023-01-17 08:14:02 +0300

    Корзина, добавление в корзину, счётчик итоговой суммы.

---
+++ OnlineShopWebApplication/Controllers/CartController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApplication.Models;
+
+namespace OnlineShopWebApplication.Controllers
+{
+    public class CartController : Controller
+    {
+        public static CartStorage cartStorage = new CartStorage();
+        public Dictionary<CartClass, int> cartList = cartStorage.GetCart();
+        public ProductStorage productStorage;
+        // GET: CartController
+        public ActionResult Index()
+        {
+            return View(cartList);
+        }
+
+        public ActionResult Add(int ID)
+        {
+            productStorage = new ProductStorage();
+            cartStorage.AddToCart(new CartClass(productStorage.TryGetByID(ID).Name, productStorage.TryGetByID(ID).Cost, 1));
+            return View("Index", cartList);
+        }
+
+        // GET: CartController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CartController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CartController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CartController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CartController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CartController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CartController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OnlineShopWebApplication/Models/CartClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApplication.Models
+{
+    public class CartClass
+    {
+        public string Name;
+        public decimal Cost;
+
+        public CartClass(string name, decimal cost, int count)
+        {
+            Name = name;
+            Cost = cost;
+        }
+    }
+}
