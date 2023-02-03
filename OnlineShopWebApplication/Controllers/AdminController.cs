4cb2926dd4ef38bb1057e247b1f3f484874320ec
Дмитрий Савченко <ilr1969@yandex.ru>
2023-02-03 12:40:29 +0300

    Добавлена админка.

---
+++ OnlineShopWebApplication/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
+        // GET: AdminController/Products/
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        // GET: AdminController/Users
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        // GET: AdminController/Roles
+        public ActionResult Roles()
+        {
+            return View();
+        }
+    }
+}
